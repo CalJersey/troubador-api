8d2594d50df301483dcc230d220b31e0df64a369
CalJersey <crippe@gmail.com>
2017-08-28 04:35:06 -0700

    changed port

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ router.delete('/posts/:id', function(req, res) {
 
 
 //start server
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 5001;
 app.listen(port, function() {
   console.log(`api running on port ${port}`);
 });
