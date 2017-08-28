91ce56126d25adb162e14a767a6a6451581581d2
CalJersey <crippe@gmail.com>
2017-08-28 05:30:44 -0700

    resolved route name error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
-import routes from "./routes.js";
+import routes from "./new_routes.js";
 
 
 //ReactDOM.render(<Main />, document.getElementById("root"));
